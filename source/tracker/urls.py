349bb807ff981a93b367e7568ec7c8476949e3b9
Aliya <israilova.aliya13@gmail.com>
2026-06-11 18:00:16 +0600

    Добавлен базовый шаблон, список и просмотр задач на TemplateView

--- source/tracker/urls.py
+++ source/tracker/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from webapp.views import TaskListView, TaskDetailView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TaskListView.as_view(), name='task_list'),
+    path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
 
-# Create your views here.
+from webapp.models import Task
+
+
+class TaskListView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all().order_by('-created_at')
+        return context
+
+
+class TaskDetailView(TemplateView):
+    template_name = 'task_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs.get('pk')
+        context['task'] = get_object_or_404(Task, pk=pk)
+        return context
