1be3579935bab2ca279b3fc23156e395984aa652
Aliya <israilova.aliya13@gmail.com>
2026-06-11 17:32:56 +0600

    Добавлены модели Status, Type, Task и миграция, регистрация в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from webapp.models import Status, Type, Task
 
-# Register your models here.
+
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'summary', 'status', 'type', 'created_at']
+    list_filter = ['status', 'type']
+    search_fields = ['summary', 'description']
+
+
+admin.site.register(Status, StatusAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Task, TaskAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class Status(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    summary = models.CharField(max_length=200, verbose_name='Краткое описание')
+    description = models.TextField(blank=True, default='', verbose_name='Полное описание')
+    status = models.ForeignKey(
+        Status,
+        on_delete=models.PROTECT,
+        related_name='tasks',
+        verbose_name='Статус',
+    )
+    type = models.ForeignKey(
+        Type,
+        on_delete=models.PROTECT,
+        related_name='tasks',
+        verbose_name='Тип задачи',
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время обновления')
+
+    def __str__(self):
+        return f'{self.pk}. {self.summary}'
